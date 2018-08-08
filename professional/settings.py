c1381296f578f70262986f1aa4edacb4112b6699
Joshua Ra'anan <joshua.raanan@gmail.com>
2018-08-08 12:08:42 -0400

    Setup MEDIA settings for image uploads

--- professional/settings.py
+++ professional/settings.py
@@ -128,6 +128,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+# MEDIA Variables are used for user uploaded images
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
--- website/urls.py
+++ website/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
   url(r'^resume/$', views.ResumeView.as_view(), name='resume'),
   url(r'^projects/$', views.ProjectAllView.as_view(), name='project-all'),
   url(r'^projects/(?P<slug>[a-zA-Z0-9_-]+)/$', views.ProjectView.as_view(), name='project'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
